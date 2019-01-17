1bcf89ce59702f0b711bc0ad0530d6dc2ee28355
jonathanmusila <jonathanmusila6@gmail.com>
2019-01-18 00:25:18 +0300

    [Feature #163267182] Added the constructro and class

--- app/api/v2/models/user.py
+++ app/api/v2/models/user.py
@@ -11,3 +11,13 @@ from app.api.v2.db_config import conn
 
 # cursor to perform database operations
 cur = conn.cursor()
+
+class User:
+    """ User constructor method """
+    def __init__(self, fname, lname, email, password, isadmin):
+        self.fname = fname
+        self.lname = lname
+        self.email = email
+        self.password = generate_password_hash(password)
+        self.isadmin = False
+        self.time_created = datetime.now()
