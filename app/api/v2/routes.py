76371537a3090ee72669a2638f9765507adab158
jonathanmusila <jonathanmusila6@gmail.com>
2019-01-18 00:47:02 +0300

    [Feature #163267182] Added the right namespaces

--- app/api/v2/routes.py
+++ app/api/v2/routes.py
@@ -8,7 +8,6 @@ from flask_restplus import Api
 
 #Local imports
 from .views.user import v2 as users_route
-from .views.meetups import v2 as meetup_route
 
     
 version2 = Blueprint('version2', __name__, url_prefix="/api/v2")
@@ -21,5 +20,4 @@ v2 = api.namespace(
     'v2')
 
 api.add_namespace(users_route, path = "/auth")
-api.add_namespace(meetup_route, path = "/meetups/upcoming")
 
