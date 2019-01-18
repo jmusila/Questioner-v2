54376d0260dcfd90826e214b8c23dca88e927cb4
jonathanmusila <jonathanmusila6@gmail.com>
2019-01-18 07:01:44 +0300

    [Feature #163267182] Added the right url

--- app/tests/test_auth.py
+++ app/tests/test_auth.py
@@ -8,15 +8,19 @@ class TestUser(Settings):
     register ={
           "lname": "musila",
           "fname": "jonathan",
-          "email": "string@gmail.com",
+          "email": "test@gmail.com",
           "password": "123jonathan"
     }
 
+    login = {
+          "email": "test@gmail.com",
+          "password": "123jonathan"
+    }
     def test_signup(self):
         """
         Test register a user
         """
-        res = self.app.post('api/v1/auth/signup', data=json.dumps(self.register), content_type='application/json')
+        res = self.app.post('api/v2/auth/signup', data=json.dumps(self.register), content_type='application/json')
         res1 = json.loads(res.data.decode())
         self.assertEqual(res1['Message'], 'User registered successfully')
         self.assertEqual(res.status_code, 201)
