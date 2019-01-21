c18d82ebd14fb66776fa3daeda4949670fea5388
jonathanmusila <jonathanmusila6@gmail.com>
2019-01-21 22:38:01 +0300

    [Feature #163284793] Added add new meetupmethod

--- app/api/v2/models/meetups.py
+++ app/api/v2/models/meetups.py
@@ -15,3 +15,19 @@ class Meetup:
         self.title = title
         self.happeningOn = happeningOn
         self.tags = tags
+   
+    """ Method creating a meetup """
+    def add_new_meetup(self):
+        user = """ INSERT INTO meetups (location, images, title, happeningOn, tags, time_added) 
+        VALUES ('{}','{}','{}','{}','{}', '{}') """\
+        .format(self.location, self.images, self.title, self.tags, self.happeningOn, self.time_added)
+        cur.execute(user)
+        conn.commit()
+
+    def meetup_data(self):
+        return dict(location =self.location, 
+                    images = self.images,
+                    title = self.title,
+                    tags = self.tags,
+                    happeningOn = self.happeningOn,
+                    time_added = self.time_added)
