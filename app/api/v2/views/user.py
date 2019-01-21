c446520d66d342c48613ec5e3432c3b0f07f45a6
jonathanmusila <jonathanmusila6@gmail.com>
2019-01-21 21:30:54 +0300

    [Starts #163267331] Added the right route

--- app/api/v2/views/user.py
+++ app/api/v2/views/user.py
@@ -2,13 +2,16 @@
 from flask_restplus import Resource
 from flask import request, abort
 from werkzeug.security import check_password_hash 
+from app.api.v2.db_config import conn
 from .helpers import get_user_by_email, get_user_by_username
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required,get_raw_jwt
 
 #Local imports
 from app.api.v2.models.user import User
-from app.api.v2.views.expect import UserRegister
+from app.api.v2.views.expect import UserRegister, UserLogin
 from app.api.common.validators import valid_email, new_user_validator
+
+cur = conn.cursor()
 
 new_user = UserRegister().users
 v2 = UserRegister().v2
