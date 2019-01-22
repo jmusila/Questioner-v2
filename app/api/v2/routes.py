3d6484b318978da15a15167daae3942bc253d5b6
jonathanmusila <jonathanmusila6@gmail.com>
2019-01-22 11:35:34 +0300

    [Feature #163284790] Registered the namespace

--- app/api/v2/routes.py
+++ app/api/v2/routes.py
@@ -9,6 +9,7 @@ from flask_restplus import Api
 #Local imports
 from .views.user import v2 as users_route
 from .views.meetups import v2 as meetup_route
+from .views.questions import v2 as quiz_route
 
 authorizations = {
     'apikey': {
@@ -28,4 +29,5 @@ v2 = api.namespace(
 
 api.add_namespace(users_route, path = "/auth")
 api.add_namespace(meetup_route, path = "/meetups/upcoming")
+api.add_namespace(quiz_route, path = "/meetups")
 
