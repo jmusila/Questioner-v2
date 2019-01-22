7bce7035b44174cc5344358e11f2386e2392f638
jonathanmusila <jonathanmusila6@gmail.com>
2019-01-22 13:23:56 +0300

    [Finishes #163284790] created a get method

--- app/api/v2/views/helpers.py
+++ app/api/v2/views/helpers.py
@@ -20,3 +20,13 @@ def get_user_by_username(username):
     cur.execute("SELECT * FROM users WHERE username='{}';".format(username))
     user = cur.fetchone()
     return user
+
+def get_question_by_id(id):
+    cur.execute("SELECT * FROM questions WHERE id='{}';".format(id))
+    qsn = cur.fetchone()
+    return qsn
+
+def get_meetup_by_id(id):
+    cur.execute("SELECT * FROM meetups WHERE id='{}';".format(id))
+    mtup = cur.fetchone()
+    return mtup
