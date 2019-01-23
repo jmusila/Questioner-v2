3e379c7491669f7e7502958f1582bb7a43010009
jonathanmusila <jonathanmusila6@gmail.com>
2019-01-23 14:35:37 +0300

    [Feature #163419609] Added the create admin method

--- app/apps.py
+++ app/apps.py
@@ -12,7 +12,7 @@ from flask import Flask
 from flask_jwt_extended import JWTManager
 
 # Local imports
-from .api.v2.db_config import create_tables, drop_all
+from .api.v2.db_config import create_tables, drop_all, create_admin
 from instance.config import app_config
 from .api.v2.db_config import conn
 from .api.v2.models.user import User
@@ -26,7 +26,7 @@ def create_app(config_name):
     app.url_map.strict_slashes = False 
 
     create_tables()
-    # User.create_admin()
+    create_admin()
     app.config['JWT_SECRET_KEY'] = secret_key
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=2)
     app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access']
