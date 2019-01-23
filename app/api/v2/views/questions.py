785d0c26f74922969a86ddfa47e5eefe827252e0
jonathanmusila <jonathanmusila6@gmail.com>
2019-01-23 06:38:19 +0300

    [Starts #163284788] Added the route

--- app/api/v2/views/questions.py
+++ app/api/v2/views/questions.py
@@ -62,4 +62,28 @@ class UpVoteQuestion(Resource):
         conn.commit()
         msg = 'You have liked this question'
         return {'Message': msg}, 200 
+
+@v2.route('/questions/<int:id>/downvote')
+class DownvoteQuestion(Resource):
+    @v2.expect(new_qsn, validate = True)
+    @v2.doc(security='apikey')
+    @jwt_required
+    def patch(self, id):
+        ''' Down votes '''
+        user = get_user_by_email(get_jwt_identity())
+        if user:
+        	user_id = user[0]
+        qsn = get_question_by_id(id)
+        if not qsn or qsn[0] != id:
+            msg = 'Question with that id does not exist'
+            return {"Message":msg},404
+        question_id = qsn[0]
+        votes = qsn[3] -1
+        p_votes = """ INSERT INTO votes (user_id, question_id, votes) 
+        VALUES ('{}','{}','{}') """\
+        .format(user_id, question_id, votes)
+        cur.execute(p_votes)
+        conn.commit()
+        msg = 'You have disliked this question'
+        return {'Message': msg}, 200
         
