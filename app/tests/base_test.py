28de9b1da31edc4a9d33abebac721f6f2a558ae2
jonathanmusila <jonathanmusila6@gmail.com>
2019-01-24 16:26:27 +0300

    [Starts #163453692] Added the base test

--- app/tests/base_test.py
+++ app/tests/base_test.py
@@ -1,4 +1,7 @@
-""" Base tests module """
+"""
+This will contain the base tests configuration.
+Thi will be reused/imported in almost all the tests.
+"""
 
 import unittest
 import json
@@ -11,12 +14,24 @@ from app.api.v2.db_config import create_tables, drop_all
 config_name = "testing"
 app = create_app(config_name)
 
+login_url = "/api/v2/auth/login"
+signup_url = "/api/v2/auth/signup"
+
+
 class Settings(unittest.TestCase):
-    def setUp(self):
-        app.testing = True
-        self.app = app.test_client()
-        create_tables()
 
+	login_data = {
+	"email": "admin@super.com",
+	"password": "isAdmin"
+	}
 
-    def tearDown(self):
-        drop_all()
+	def setUp(self):
+		app.testing = True
+		self.app = app.test_client()
+		create_tables()
+
+	def give_token(self):
+		login = self.app.post(login_url, data=json.dumps(self.login_data), content_type='application/json')	
+		login_data = json.loads(login.data.decode())
+		return login_data["access_token"]
+        
