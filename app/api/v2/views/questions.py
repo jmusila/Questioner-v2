1ea515448d35450e6706fd79574d9f31f130ed80
jonathanmusila <jonathanmusila6@gmail.com>
2019-02-13 11:35:45 +0300

    [Starts #163935599] Started get all questions

--- app/api/v2/views/questions.py
+++ app/api/v2/views/questions.py
@@ -42,6 +42,32 @@ class AddQuestion(Resource):
             return {'Status': 201, 'Message': "Question posted successfully", 'Question': qsn}, 201
         return question_validator(data)
 
+    def get(self, id):
+        """
+        Get all questions for a specific meetup
+        """
+        cur.execute(
+            "SELECT * FROM questions WHERE meetup_id={};".format(id))
+        questions = cur.fetchall()
+        mtup = get_meetup_by_id(id)
+        all_questions = []
+        for item in questions:
+            format_quiz = {'question_id': item[0],
+                        'user_id': item[1],
+                        'meetup_id': item[2],
+                        'votes': item[3],
+                        'title': item[4],
+                        'body': item[5],
+                        'time_added':str(item[6])}
+            all_questions.append(format_quiz)
+        if not mtup or mtup[0] != id:
+            msg = 'Meetup with that id does not exist'
+            return {"Status":404, "Message":msg},404
+        if len(all_questions) < 1:
+            res= {"Status":404,"Message":"There are no Questions at the moment"},404
+            return res
+        return {"Status": 200, "data": all_questions}, 200
+
 @v2.route('/questions/<int:id>/upvote')
 class UpVoteQuestion(Resource):
     @v2.doc(security='apikey')
